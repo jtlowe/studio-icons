57dc3ffbdb4190d6731b2bc2cb39e72821089143
Jordan Lowe <jtlowe@users.noreply.github.com>
2016-10-19 07:07:03 -0500

    Trying to add in support for pdfs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
+var fs = require("fs");
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
+var pdfjs = require('pdfjs-dist');
 
 var colors = {
     defaultFg: "424242",
@@ -12,13 +14,6 @@ var colors = {
     darkFg: "C5C5C5",
     darkBg: "252526"
 };
-
-gulp.task('ai', function () {
-    return gulp.src([
-        'src/icons/*.ai',
-    ])
-        .pipe(gulp.dest('temp/test_ai'));
-});
 
 gulp.task('saveDark', function () {
     return gulp.src([
@@ -45,4 +40,65 @@ gulp.task('saveLight', function () {
         .pipe(gulp.dest('fileicons/images'));
 });
 
-gulp.task('default', ['saveDark', 'saveLight']);
+gulp.task('convertAi', function () {
+    var pdfPath = './src/ai/Certificate_16x.ai';
+    var data = new Uint8Array(fs.readFileSync(pdfPath));
+    // pdfjs.disableWorker = true;
+    pdfjs.workerSrc = true;
+
+    pdfjs.getDocument(data).then(function (doc) {
+        console.log('# Document Loaded');
+        // console.log('Number of Pages: ' + numPages);
+        // console.log();
+
+        var lastPromise = Promise.resolve(); // will be used to chain promises
+        var loadPage = function (pageNum) {
+            return doc.getPage(pageNum).then(function (page) {
+                var viewport = page.getViewport(1.0 /* scale */);
+                console.log('page loaded');
+
+                return page.getOperatorList().then(function (opList) {
+                    console.log('got operator list')
+                    var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
+
+                    var test = svgGfx.getSVG(opList, viewport);
+
+                    console.log(test)
+
+                    // svgGfx.embedFonts = true;
+                    // return svgGfx.getSVG(opList, viewport).then(function (svg) {
+                    //     console.log('got svg')
+                    //     //var svgDump = svg.toString();
+                    //     //writeToFile(svgDump);
+                    // });
+                });
+            });
+        };
+
+        // for (var i = 1; i <= numPages; i++) {
+        lastPromise = lastPromise.then(loadPage.bind(null, 1));
+        // }
+        return lastPromise;
+    })
+});
+
+function writeToFile(svgdump) {
+    var name = getFileNameFromPath(pdfPath);
+    fs.mkdir('./temp/', function (err) {
+        if (!err || err.code === 'EEXIST') {
+            fs.writeFile('./svgdump/' + name + '.svg', svgdump,
+                function (err) {
+                    if (err) {
+                        console.log('Error: ' + err);
+                    } else {
+                        console.log('Page: ' + pageNum);
+                    }
+                });
+        }
+    });
+}
+
+
+
+
+// gulp.task('default', ['saveDark', 'saveLight']);

e609917d8e2410104bc3f6c00ad54520817309a3
Jordan Lowe <jtlowe@users.noreply.github.com>
2016-10-18 19:56:30 -0500

    Moving to subfolder

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,42 @@ var gulp = require('gulp');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 
-var defaultFgColor = "#111111";
-var defaultBgColor = "#111111";
-var lightFgColor = "#656565";
-var lightBgColor = "#F3F3F3";
-var darkFgColor = "#C5C5C5";
-var darkBgColor = "#252526";
+var colors = {
+    defaultFg: "#424242",
+    defaultBg: "#f6f6f6",
+    lightFg: "#656565",
+    lightBg: "#F3F3F3",
+    darkFg: "#C5C5C5",
+    darkBg: "#252526"
+};
 
 gulp.task('ai', function () {
-  return gulp.src([
-      'src/icons/*.ai',
-     ])
-    .pipe(gulp.dest('temp/test_ai'));
+    return gulp.src([
+        'src/icons/*.ai',
+    ])
+        .pipe(gulp.dest('temp/test_ai'));
 });
 
-gulp.task('svg', function () {
+gulp.task('saveDark', function () {
+    return gulp.src([
+        'src/svg/*.svg'
+    ])
+        .pipe(replace(color.defaultFg, color.darkFg))
+        .pipe(replace(color.defaultBg, color.darkBg))
+        .pipe(rename(function (path) {
+            path.basename += "_inverse";
+        }))
+        .pipe(gulp.dest('temp/test_svg'));
+});
+
+gulp.task('saveLight', function () {
     return gulp.src([
         'fileicons/images/*.svg',
         '!fileicons/images/*_inverse.svg',
     ])
-    .pipe(replace(lightFgColor, darkFgColor))
-    .pipe(replace(lightBgColor, darkBgColor))
-    .pipe(rename(function (path) {
-        path.basename += "_inverse";
-    }))
-    .pipe(gulp.dest('temp/test_svg'));
+        .pipe(replace(color.defaultFg, color.lightFg))
+        .pipe(replace(color.defaultBg, color.lightFg))
+        .pipe(gulp.dest('temp/test_svg'));
 });
 
-gulp.task('default', ['svg']);
+gulp.task('default', ['saveLight', 'saveDark']);

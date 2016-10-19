06365a672150195923803b128916b116e94d65d2
Jordan Lowe <jordan@noreply.jtlowe.com>
2016-10-19 14:33:36 -0500

    Tying some stuff out

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var fs = require("fs");
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
-var pdfjs = require('pdfjs-dist');
+var PDFJS = require('pdfjs-dist');
+var Canvas = require('canvas');
+
+// PDFJS.disableWorker = true;
+// PDFJS.workerSrc = 'node_modules/-dist/build/pdf.worker.js';
 
 var colors = {
     defaultFg: "424242",
@@ -41,45 +45,83 @@ gulp.task('saveLight', function () {
 });
 
 gulp.task('convertAi', function () {
-    var pdfPath = './src/ai/Certificate_16x.ai';
+    // var pdfPath = './src/ai/Certificate_16x.ai';
+    var pdfPath = './test/tracemonkey.pdf';
+
     var data = new Uint8Array(fs.readFileSync(pdfPath));
-    // pdfjs.disableWorker = true;
-    pdfjs.workerSrc = true;
+    // PDFJS.disableWorker = true;
+    // PDFJS.workerSrc = true;
 
-    pdfjs.getDocument(data).then(function (doc) {
-        console.log('# Document Loaded');
-        // console.log('Number of Pages: ' + numPages);
-        // console.log();
 
-        var lastPromise = Promise.resolve(); // will be used to chain promises
-        var loadPage = function (pageNum) {
-            return doc.getPage(pageNum).then(function (page) {
-                var viewport = page.getViewport(1.0 /* scale */);
-                console.log('page loaded');
-
-                return page.getOperatorList().then(function (opList) {
-                    console.log('got operator list')
-                    var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
-
-                    var test = svgGfx.getSVG(opList, viewport);
-
-                    console.log(test)
-
-                    // svgGfx.embedFonts = true;
-                    // return svgGfx.getSVG(opList, viewport).then(function (svg) {
-                    //     console.log('got svg')
-                    //     //var svgDump = svg.toString();
-                    //     //writeToFile(svgDump);
-                    // });
-                });
-            });
-        };
-
-        // for (var i = 1; i <= numPages; i++) {
-        lastPromise = lastPromise.then(loadPage.bind(null, 1));
-        // }
-        return lastPromise;
+    // Asynchronous download PDF
+    PDFJS.getDocument(data)
+    .then(function(pdf) {
+        console.log('then #1')
+        return pdf.getPage(1);
     })
+    .then(function(page) {
+        console.log('then #2')
+        // console.log(page)
+
+
+        page
+        .getOperatorList()
+        .then(function (opList) {
+            var viewport = page.getViewport(1.0 /* scale */);
+            console.log('page then #1')
+            var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
+
+            try{
+                return svgGfx.getSVG(opList, viewport);
+            }
+            catch(e){
+                console.log(e)
+            }
+
+        })
+        .then(function (svg) {
+            console.log('page then #2')
+            
+            // container.appendChild(svg);
+        });
+
+    });
+
+
+    // PDFJS.getDocument(data).then(function (doc) {
+    //     console.log('# Document Loaded');
+    //     // console.log('Number of Pages: ' + numPages);
+    //     // console.log();
+
+    //     var lastPromise = Promise.resolve(); // will be used to chain promises
+    //     var loadPage = function (pageNum) {
+    //         return doc.getPage(pageNum).then(function (page) {
+    //             var viewport = page.getViewport(1.0 /* scale */);
+    //             console.log('page loaded');
+
+    //             return page.getOperatorList().then(function (opList) {
+    //                 console.log('got operator list')
+    //                 var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
+
+    //                 var test = svgGfx.getSVG(opList, viewport);
+
+    //                 console.log(svgGfx.getSVG(opList, viewport))
+
+    //                 // svgGfx.embedFonts = true;
+    //                 return svgGfx.getSVG(opList, viewport).then(function (svg) {
+    //                     console.log('got svg')
+    //                     //var svgDump = svg.toString();
+    //                     writeToFile(svgDump);
+    //                 });
+    //             });
+    //         });
+    //     };
+
+    //     // for (var i = 1; i <= numPages; i++) {
+    //     lastPromise = lastPromise.then(loadPage.bind(null, 1));
+    //     // }
+    //     return lastPromise;
+    // })
 });
 
 function writeToFile(svgdump) {

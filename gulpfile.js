bba88d4415f14c8e57d6633ed819c18f546a46d6
Jordan Lowe <jordan@noreply.jtlowe.com>
2016-10-19 16:05:28 -0500

    checking

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,10 @@ var fs = require("fs");
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
-var PDFJS = require('pdfjs-dist');
-var Canvas = require('canvas');
+var inkscape = require('inkscape');
+var mappings = require('./src/mappings.json');
 
-// PDFJS.disableWorker = true;
-// PDFJS.workerSrc = 'node_modules/-dist/build/pdf.worker.js';
+var svgToPdfConverter = new inkscape(['--export-pdf', '--export-width=1024']);
 
 var colors = {
     defaultFg: "424242",
@@ -23,124 +22,70 @@ gulp.task('saveDark', function () {
     return gulp.src([
         'src/svg/*.svg'
     ])
-        .pipe(replace(colors.defaultFg, colors.darkFg))
-        .pipe(replace(colors.defaultBg, colors.darkBg))
-        .pipe(replace(colors.defaultBg2, colors.darkBg))
-        .pipe(replace(colors.defaultBg3, colors.darkBg))
-        .pipe(rename(function (path) {
-            path.basename += "_inverse";
-        }))
-        .pipe(gulp.dest('fileicons/images'));
+    .pipe(replace(colors.defaultFg, colors.darkFg))
+    .pipe(replace(colors.defaultBg, colors.darkBg))
+    .pipe(replace(colors.defaultBg2, colors.darkBg))
+    .pipe(replace(colors.defaultBg3, colors.darkBg))
+    .pipe(rename(function (path) {
+        path.basename += "_inverse";
+    }))
+    .pipe(gulp.dest('fileicons/images'));
 });
 
 gulp.task('saveLight', function () {
     return gulp.src([
         'src/svg/*.svg'
     ])
-        .pipe(replace(colors.defaultFg, colors.lightFg))
-        .pipe(replace(colors.defaultBg, colors.lightBg))
-        .pipe(replace(colors.defaultBg2, colors.lightBg))
-        .pipe(replace(colors.defaultBg3, colors.lightBg))
-        .pipe(gulp.dest('fileicons/images'));
+    .pipe(replace(colors.defaultFg, colors.lightFg))
+    .pipe(replace(colors.defaultBg, colors.lightBg))
+    .pipe(replace(colors.defaultBg2, colors.lightBg))
+    .pipe(replace(colors.defaultBg3, colors.lightBg))
+    .pipe(gulp.dest('fileicons/images'));
 });
 
-gulp.task('convertAi', function () {
-    // var pdfPath = './src/ai/Certificate_16x.ai';
-    var pdfPath = './test/tracemonkey.pdf';
+gulp.task('mappings', function () {
+    var savePath = "./temp/";
+    var themeSettings = {
+        iconDefinitions: {}
+    };
 
-    var data = new Uint8Array(fs.readFileSync(pdfPath));
-    // PDFJS.disableWorker = true;
-    // PDFJS.workerSrc = true;
+    themeSettings.iconDefinitions = createDefinitionsObj();
+    
+    // console.log(themeSettings)
 
+    if (!fs.existsSync(savePath)){
+        fs.mkdirSync(savePath);
+    }
 
-    // Asynchronous download PDF
-    PDFJS.getDocument(data)
-    .then(function(pdf) {
-        console.log('then #1')
-        return pdf.getPage(1);
-    })
-    .then(function(page) {
-        console.log('then #2')
-        // console.log(page)
-
-
-        page
-        .getOperatorList()
-        .then(function (opList) {
-            var viewport = page.getViewport(1.0 /* scale */);
-            console.log('page then #1')
-            var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
-
-            try{
-                return svgGfx.getSVG(opList, viewport);
-            }
-            catch(e){
-                console.log(e)
-            }
-
-        })
-        .then(function (svg) {
-            console.log('page then #2')
-            
-            // container.appendChild(svg);
-        });
-
+    fs.writeFile(savePath + 'vscode-icon-theme.json', JSON.stringify(themeSettings), function (err) {
+        if (err) return console.log(err);
+        console.log("file saved");
     });
 
-
-    // PDFJS.getDocument(data).then(function (doc) {
-    //     console.log('# Document Loaded');
-    //     // console.log('Number of Pages: ' + numPages);
-    //     // console.log();
-
-    //     var lastPromise = Promise.resolve(); // will be used to chain promises
-    //     var loadPage = function (pageNum) {
-    //         return doc.getPage(pageNum).then(function (page) {
-    //             var viewport = page.getViewport(1.0 /* scale */);
-    //             console.log('page loaded');
-
-    //             return page.getOperatorList().then(function (opList) {
-    //                 console.log('got operator list')
-    //                 var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
-
-    //                 var test = svgGfx.getSVG(opList, viewport);
-
-    //                 console.log(svgGfx.getSVG(opList, viewport))
-
-    //                 // svgGfx.embedFonts = true;
-    //                 return svgGfx.getSVG(opList, viewport).then(function (svg) {
-    //                     console.log('got svg')
-    //                     //var svgDump = svg.toString();
-    //                     writeToFile(svgDump);
-    //                 });
-    //             });
-    //         });
-    //     };
-
-    //     // for (var i = 1; i <= numPages; i++) {
-    //     lastPromise = lastPromise.then(loadPage.bind(null, 1));
-    //     // }
-    //     return lastPromise;
-    // })
 });
 
-function writeToFile(svgdump) {
-    var name = getFileNameFromPath(pdfPath);
-    fs.mkdir('./temp/', function (err) {
-        if (!err || err.code === 'EEXIST') {
-            fs.writeFile('./svgdump/' + name + '.svg', svgdump,
-                function (err) {
-                    if (err) {
-                        console.log('Error: ' + err);
-                    } else {
-                        console.log('Page: ' + pageNum);
-                    }
-                });
+gulp.task('convert', ['saveDark', 'saveLight']);
+
+
+function createDefinitionsObj() {
+    var defs = {};
+    var relPath = mappings.settings.iconRelPath;
+    var darkPostfix = mappings.settings.darkTheme.postfix;
+    var iconExtension = mappings.settings.iconFileExtension;
+
+    for(var i = 0; i < mappings.iconDefinitions.length; i++){
+        var def = mappings.iconDefinitions[i];
+        var iconName = def.icon;
+        var iconNameDark = def.icon.replace(iconExtension, darkPostfix + iconExtension)
+        
+        defs[iconName] = {
+            "iconPath": relPath + iconName
         }
-    });
+
+        defs[iconNameDark] = {
+            "iconPath": relPath + iconNameDark
+        }
+    }
+
+    return defs;
 }
-
-
-
-
-// gulp.task('default', ['saveDark', 'saveLight']);

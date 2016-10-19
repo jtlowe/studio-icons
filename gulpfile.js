0a8a9417f314ca83cb50f82ff7d89c40ac956611
Jordan Lowe <jtlowe@users.noreply.github.com>
2016-10-18 21:33:46 -0500

    Outputting from source sag

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 
 var colors = {
-    defaultFg: "#424242",
-    defaultBg: "#f6f6f6",
-    lightFg: "#656565",
-    lightBg: "#F3F3F3",
-    darkFg: "#C5C5C5",
-    darkBg: "#252526"
+    defaultFg: "424242",
+    defaultBg: "F6F6F6",
+    defaultBg2: "F0EFF1",
+    defaultBg3: "EFEEF0",
+    lightFg: "656565",
+    lightBg: "F3F3F3",
+    darkFg: "C5C5C5",
+    darkBg: "252526"
 };
 
 gulp.task('ai', function () {
@@ -22,22 +24,25 @@ gulp.task('saveDark', function () {
     return gulp.src([
         'src/svg/*.svg'
     ])
-        .pipe(replace(color.defaultFg, color.darkFg))
-        .pipe(replace(color.defaultBg, color.darkBg))
+        .pipe(replace(colors.defaultFg, colors.darkFg))
+        .pipe(replace(colors.defaultBg, colors.darkBg))
+        .pipe(replace(colors.defaultBg2, colors.darkBg))
+        .pipe(replace(colors.defaultBg3, colors.darkBg))
         .pipe(rename(function (path) {
             path.basename += "_inverse";
         }))
-        .pipe(gulp.dest('temp/test_svg'));
+        .pipe(gulp.dest('fileicons/images'));
 });
 
 gulp.task('saveLight', function () {
     return gulp.src([
-        'fileicons/images/*.svg',
-        '!fileicons/images/*_inverse.svg',
+        'src/svg/*.svg'
     ])
-        .pipe(replace(color.defaultFg, color.lightFg))
-        .pipe(replace(color.defaultBg, color.lightFg))
-        .pipe(gulp.dest('temp/test_svg'));
+        .pipe(replace(colors.defaultFg, colors.lightFg))
+        .pipe(replace(colors.defaultBg, colors.lightBg))
+        .pipe(replace(colors.defaultBg2, colors.lightBg))
+        .pipe(replace(colors.defaultBg3, colors.lightBg))
+        .pipe(gulp.dest('fileicons/images'));
 });
 
-gulp.task('default', ['saveLight', 'saveDark']);
+gulp.task('default', ['saveDark', 'saveLight']);
